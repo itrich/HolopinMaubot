af713f66cf66ccad2dcf9fbea517e9528578e2e5
Eduard Itrich <eduard@itrich.net>
2023-02-02 13:14:42 +0100

    The bot now sends the claim URL to the given user(s)
    Signed-off-by: Eduard Itrich <eduard@itrich.net>

--- holopin.py
+++ holopin.py
@@ -13,6 +13,7 @@ class Config(BaseProxyConfig):
     helper.copy("whitelist")
 
 class HolopinPlugin(Plugin):
+
   async def start(self) -> None:
     self.config.load_and_update()
 
@@ -32,15 +33,31 @@ class HolopinPlugin(Plugin):
       users = argument_parts[1:]
     else:
       await event.reply(f"Please provide a stickerId and at least one Matrix user that shall be awarded.")
+      return None
 
     if event.sender in self.config["whitelist"]:
       api_key = self.config["api_key"]
       url_params = urllib.parse.urlencode({"id": sticker_id, "apiKey": api_key})
       url = "https://www.holopin.io/api/sticker/share?{}".format(url_params)
-      response = await self.http.post(url, json={"email": users[0]})
-      self.log.debug(f"Call {url}: {response.status} – {await response.text()}")
+      for user in users:
+        response = await self.http.post(url, json={"email": ""})
+        if response.status == 200:
+          success_response = await response.json()
+          claim_id = success_response['data']['id']
+          claim_url = "https://holopin.io/claim/" + claim_id
+          # Send mesage to a private chat
+          # TODO: This will create a new room with every call. Reusing an old private chat is not trivial with Matrix.
+          room_id = await self.client.create_room(preset=RoomCreatePreset.TRUSTED_PRIVATE, invitees=[user], is_direct=True)
+          await self.client.send_notice(room_id, text=f"Congratulations! Claim your brand new badge at {claim_url}")
+          # Confirm success to the issuer
+          await event.reply(f"Successfully sent badge to {user}.")
+          return None
+        else:
+          await event.reply(f"Issuing the regular badge {sticker_id} failed wit error {response.status}.")
+          return None
     else:
       await event.reply(f"You're not allowed to issue badges.")
+      return None
 
   @classmethod
   def get_config_class(cls) -> Type[BaseProxyConfig]:
