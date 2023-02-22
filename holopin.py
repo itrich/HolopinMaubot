5c5e0fec025e6f15e900e6d6a69eab7a49e83745
Eduard Itrich <eduard@itrich.net>
2023-02-22 15:32:19 +0100

    Fix wording and incorporate feedback by @elenalape
    Signed-off-by: Eduard Itrich <eduard@itrich.net>

--- holopin.py
+++ holopin.py
@@ -10,7 +10,8 @@ class Config(BaseProxyConfig):
 
   def do_update(self, helper: ConfigUpdateHelper) -> None:
     helper.copy("api_key")
-    helper.copy("whitelist")
+    helper.copy("issuers")
+    helper.copy("badges")
 
 class HolopinPlugin(Plugin):
 
@@ -21,23 +22,32 @@ class HolopinPlugin(Plugin):
   async def holopin(self, event: MessageEvent) -> None:
     pass    
 
-  @holopin.subcommand(help="Issue a regular badge to a list of users")
+  @holopin.subcommand(help="Award a badge to a list of users, e.g. !holopin award hackathon @user1:matrix.org @user2:matrix.org")
   @command.argument("argument", pass_raw=True, required=True)
-  async def issue_regular(self, event: MessageEvent, argument: str) -> None:
+  async def award(self, event: MessageEvent, argument: str) -> None:
     await event.mark_read()
 
     if argument and len(argument.split()) >= 2:
       # Split input 
       argument_parts = argument.split()
-      sticker_id = argument_parts[0]
+      badge_alias = argument_parts[0]
       users = argument_parts[1:]
     else:
-      await event.reply(f"Please provide a stickerId and at least one Matrix user that shall be awarded.")
+      await event.reply(f"Please provide a badge alias and at least one Matrix user that shall be awarded.")
       return None
 
-    if event.sender in self.config["whitelist"]:
+    if event.sender in self.config["issuers"]:
+      # Read configured API Key
       api_key = self.config["api_key"]
-      url_params = urllib.parse.urlencode({"id": sticker_id, "apiKey": api_key})
+      # Get badge information from list of configured badge aliases
+      badges = self.config["badges"]
+      if badge_alias in badges.keys():
+        badge_id = badges[badge_alias]["id"]
+      else:
+        await event.reply(f"{badge_alias} was not found in list of available badges. Choose one from {','.join(badges.keys())}")
+        return None
+      # Construct API URL
+      url_params = urllib.parse.urlencode({"id": badge_id, "apiKey": api_key})
       url = "https://www.holopin.io/api/sticker/share?{}".format(url_params)
       for user in users:
         response = await self.http.post(url, json={"email": ""})
@@ -45,17 +55,17 @@ class HolopinPlugin(Plugin):
           success_response = await response.json()
           claim_id = success_response['data']['id']
           claim_url = "https://holopin.io/claim/" + claim_id
-          # Send mesage to a private chat
+          # Send message to a private chat
           # TODO: This will create a new room with every call. Reusing an old private chat is not trivial with Matrix.
           room_id = await self.client.create_room(preset=RoomCreatePreset.TRUSTED_PRIVATE, invitees=[user], is_direct=True)
           await self.client.send_notice(room_id, text=f"Congratulations! Claim your brand new badge at {claim_url}")
           # Confirm success to the issuer
           await event.reply(f"Successfully sent badge to {user}.")
         else:
-          await event.reply(f"Issuing the regular badge {sticker_id} failed wit error {response.status}.")
+          await event.reply(f"Issuing the badge {badge_id} failed wit error {response.status}.")
       return None
     else:
-      await event.reply(f"You're not allowed to issue badges.")
+      await event.reply(f"You're not allowed to award badges.")
       return None
 
   @classmethod
